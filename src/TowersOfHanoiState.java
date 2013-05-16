037070b7b569e64c0f39c662dbae2cb8787f9662
Roeland Singer-Heinze <roeland@umail.ucsb.edu>
2013-05-15 19:19:40 -0700

    Initial commit, outlined some of the methods with stubs

---
+++ src/TowersOfHanoiState.java
@@ -0,0 +1,54 @@
+edu.ucsb.cs56.projects.games.towers_of_hanoi.model;
+
+
+
+public class TowersOfHanoiState{
+
+
+    private boolean isGameSolved = false;
+    private int numOfMoves = 0;
+    private int [] towerZero;
+    private int [] towerOne;
+    private int [] towerTwo;
+
+
+
+    boolean isMoveLegal(int indexFromTower, int indexToTower){
+
+
+	return false; //Stub, needs implementation
+    }
+
+    void makeMove() throws java.io.IOException {
+
+
+    }
+
+    int [] disksOnTower(Tower q){
+
+
+	return null; //stub, fix it
+    }
+
+
+    String toString(){
+
+	return null; //stub, fix it
+    }
+
+    boolean solved(){
+
+	return false; // stub, fix it
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/TowersOfHanoiStateTest.java
@@ -0,0 +1,1 @@
+edu.ucsb.cs56.projects.games.towers_of_hanoi.model;
