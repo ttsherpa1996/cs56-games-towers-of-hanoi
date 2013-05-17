2cce380c3ce5f02ec63674f9f70a6ad5d13076d0
sophiamao <sophiamao@gmail.com>
2013-05-17 16:47:29 -0600

    update structure. still can't repaint properly.

--- src/edu/ucsb/cs56/projects/games/towers_of_hanoi/view/SwingHanoiJFrame.java
+++ src/edu/ucsb/cs56/projects/games/towers_of_hanoi/view/SwingHanoiJFrame.java
@@ -1,4 +1,6 @@
 package edu.ucsb.cs56.projects.games.towers_of_hanoi.view;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.*;
@@ -10,8 +12,11 @@ class SwingHanoiJFrame extends JFrame{
 	static int intFrom, intTo;
 	static String from, to;
 	static boolean init, stillGaming;
+	
 	static JPanel container = new JPanel();
 	static SwingHanoiJFrame f = new SwingHanoiJFrame();
+	
+	private JButton makeMove = new JButton("make a move");
 
 	
     public SwingHanoiJFrame() {
@@ -22,67 +27,72 @@ class SwingHanoiJFrame extends JFrame{
         setBackground(Color.WHITE);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
+  
+    }
+    
+	private void initComponents(){
+		
+		init = true;
+		stillGaming = true;
 
- 
+			container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+
+			JPanel hanoi = new HanoiJPanel(init);
+			container.add(hanoi);
+
+			JButton makeMove = new JButton("make a move");
+			container.add(makeMove);
+			
+			f.add(container);
+			
+			f.setSize(500,500);
+			f.setVisible(true);
+			
+		makeMove.addActionListener( new ActionListener(){
+			
+			public void actionPerformed(ActionEvent actionEvent){
+			
+				makeMoveButtonActionPerformed( actionEvent );
+			}
+		});
+	}
+    
+        
+    private void makeMoveButtonActionPerformed(ActionEvent actionEvent) {
+    
+    	init = false;
+    	stillGaming = true;
+    	HanoiJPanel hanoi = new HanoiJPanel(init);// TODO Auto-generated method stub
+    	updateFrame(hanoi);
+			
+    }
+
+    
+    private void updateFrame(HanoiJPanel panel){
+    	container.add(panel);
     	
+    	container.add(makeMove);
+    	stillGaming = true;
+    	
+		f.add(container);
+		f.setSize(500,500);
+		f.setVisible(true);
+    }
+    
+    public void run(){
+    	initComponents();
+    	while(stillGaming && (init==false)){
+    		f.removeAll();
+    		HanoiJPanel hanoi = new HanoiJPanel(false);
+    		updateFrame(hanoi);
+    	}
     }
 
 
     public static void main(String[] args) {
 
-	init = true;
-	stillGaming= true;
-
-
-
-
-		container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
-		
-		class ActionListener2 implements ActionListener {
-			public void actionPerformed(ActionEvent e) {
-
-					from = JOptionPane.showInputDialog("Which column do you want to move from?");
-					to = JOptionPane.showInputDialog("move to where?");
-					
-					intFrom = Integer.parseInt(from);
-					intTo = Integer.parseInt(to);
-					init = false;
-					
-					JPanel hanoi = new HanoiJPanel(intFrom, intTo);
-					container.add(hanoi);
-
-					
-
-				}
-		}
-
-		if(init == true){
-			JPanel hanoi = new HanoiJPanel();
-			container.add(hanoi);
-
-			JButton makeMove = new JButton("make a move");
-			makeMove.addActionListener(new ActionListener2());
-
-
-
-		
-						
-
-				container.add(makeMove);
-
-
-				init = false;
-
-		}
-		
-		f.add(container);
-		f.setSize(500,500);
-		f.setVisible(true);
-
-
-	//JPanel tile = new TileJPanel();
-	
-			
+    	SwingHanoiJFrame game = new SwingHanoiJFrame();
+    	game.run();		
 
 	}
 
