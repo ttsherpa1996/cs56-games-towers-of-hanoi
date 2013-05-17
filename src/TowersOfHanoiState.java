8fdf4510daa1eb76799206d8df6ff92e825e589a
Roeland Singer-Heinze <roeland@umail.ucsb.edu>
2013-05-17 12:00:52 -0700

    Added javadoc comments for methods and tests.

--- src/TowersOfHanoiState.java
+++ src/TowersOfHanoiState.java
@@ -1,5 +1,11 @@
 edu.ucsb.cs56.projects.games.towers_of_hanoi.model;
+import java.util.ArrayList;
 
+
+/** TowersOfHanoiState models the state of a game based on Towers of Hanoi.
+
+
+ */
 
 
 public class TowersOfHanoiState{
@@ -7,47 +13,98 @@ public class TowersOfHanoiState{
 
     private boolean isGameSolved = false;
     private int numOfMoves = 0;
-    private int [] towerZero;
-    private int [] towerOne;
-    private int [] towerTwo;
+    private ArrayList<ArrayList<Integer>> towers 
+	= new ArrayList<ArrayList<Integer>>(3); //the 3 towers and their disks will be stored here
+
+    /**
+     *No argument constructor which initializes a game starting with 3 discs.
+     */
+
+    public TowersOfHanoiState(){
+
+	towers = new ArrayList<ArrayList<Integer>>(); //stub
+
+    }
+
+    /**
+     *TowersOfHanoiState constructor with argument for number of disks.
+     *Should initialize game with number of disks specified.
+     *@param numOfDisks is an integer number specifying number of disks for game
+     */
+
+    public TowersOfHanoiState(int numOfDisks){
+
+	if (numOfDisks <= 3 ){ // Want a minimum of 3 disks
+
+	    towers = new ArrayList<ArrayList<Integer>>(); //stub
+
+	}
+
+	else { // else user input is number of disks
+
+	    towers = new ArrayList<ArrayList<Integer>>(); //stub
+
+	}
+
+    }
+
+    /**
+     *Given the index of two towers, this  method determines whether a given move is legal.
+     *@param indexFromTower is an integer representing which tower you want to move a disk from.
+     *@param indexToTower is an integer representing which tower you want to move a disk to.
+     *@return boolean value true if move is legal, false if illegal.
+     */
 
 
-
-    boolean isMoveLegal(int indexFromTower, int indexToTower){
+    public boolean isMoveLegal(int indexFromTower, int indexToTower){
 
 
 	return false; //Stub, needs implementation
     }
 
-    void makeMove() throws java.io.IOException {
+    /**
+     *Given the index number of two towers, this method will move the top disk from one tower to the other.
+     *Throws input/output exception if move is illegal.
+     *@param indexFromTower is an integer representing which tower you want to move a disk from.
+     *@param indexToTower is an integer representing which tower you want to move a disk to.
+     */
+
+    public void makeMove(int indexFromTower, int indexToTower) throws java.io.IOException {
 
 
     }
 
-    int [] disksOnTower(Tower q){
+    /**
+     *Given a tower, this method returns an array with the disk numbers on the tower.
+     *@param tower is an ArrayList which contains the disks on the tower
+     *@return an integer array containing the disks of the given tower
+     */
+
+    public int [] disksOnTower(ArrayList<Integer> tower){
 
 
 	return null; //stub, fix it
     }
 
+    /**
+     *A toString method that returns an ASCII art representation of the towers as a string.
+     *@return a String that represents the game state complete with newline characters.
+     */
 
-    String toString(){
+    public String toString(){
 
 	return null; //stub, fix it
     }
 
-    boolean solved(){
+    /**
+     *This is a method to determine whether the game has been solved yet.
+     *@return a boolean value true if game is solved, false otherwise.
+     */
+
+    public boolean solved(){
 
 	return false; // stub, fix it
     }
-
-
-
-
-
-
-
-
 
 
 
--- src/TowersOfHanoiStateTest.java
+++ src/TowersOfHanoiStateTest.java
@@ -1,1 +1,93 @@
 edu.ucsb.cs56.projects.games.towers_of_hanoi.model;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+/**
+ *The test class TowersOfHanoiStateTest for TowersOfHanoiState class.
+ *@author Roeland Singer-Heinze
+ *@version CS56, 13S, proj1
+ *@see TowersOfHanoiState
+ */
+
+
+public class TowersOfHanoiStateTest{
+
+    /**
+     *Test no argument constructor TowersOfHanoiState.
+     *Should initialize game with 3 starting disks.
+     *@see TowersOfHanoiState#TowersOfHanoiState
+     */
+
+    @test public void noArgumentConstructorTest(){
+
+
+    } 
+
+    /**
+     *Test constructor TowersOfHanoiState with argument for number of disks.
+     *Should initialize game with number of disks specified.
+     *@see TowersOfHanoiState#TowersOfHanoiState
+     */
+
+    @test public void constructorWithArgument1(){
+
+    }
+
+
+    @test public void constructorWithArgument2(){
+
+    }
+
+
+    /**
+     *test isMoveLegal method (returns false if move is illegal)
+     *@see TowersOfHanoiState#isMoveLegal
+     */
+
+    @test public void isMoveLegal_Test(){
+
+
+	assertTrue( );
+
+    }
+
+    /**
+     *test makeMove method, which moves disk from one tower to another.
+     *@see TowersOfHanoiState#makeMove
+     */
+
+    @test public void makeMove_Test(){
+
+    }
+
+    /**
+     *test disksOnTower method, which returns an integer array of the disks on given tower.
+     *@see TowersOfHanoiState#disksOnTower
+     */
+
+    @test public void disksOnTower_Test(){
+
+    }
+
+    /**
+     *test toString method which returns an ASCII art representation of game state.
+     *@see TowersOfHanoiState#toString
+     */
+
+    @test public void toString_Test(){
+
+    }
+
+    /**
+     *test solved method which returns boolean value indicating if game is solved.
+     *@see TowersOfHanoiState#solved
+     */
+
+    @test public void solved_Test(){
+
+    }
+
+
+}
