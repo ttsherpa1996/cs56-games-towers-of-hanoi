a679dfc1da9eca12b4f5888cf85cb5e17471049d
Shanen Cross <shanencross@umail.ucsb.edu>
2013-05-19 23:40:25 -0700

    Added framework for callback/event-handler for GUI Timer

---
+++ src/edu/ucsb/cs56/projects/games/towers_of_hanoi/utility/UpdateGUITimer.java
@@ -0,0 +1,14 @@
+package src.edu.ucsb.cs56.games.towers_of_hanoi.utility;
+
+/** Timer Callback To Be Invoked Every N Milliseconds
+ * @author Shanen Cross
+ */
+Class UpdateGUITimer implements TimerCallback {
+    
+    @Override
+	void callback(Object o) {
+	if (o != null)
+	    {
+		// Implement rendering of GUI Timer here
+	    }
+}
