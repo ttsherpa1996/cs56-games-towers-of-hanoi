112c6de121c0fd1d78872a0bf58c6e9e5c620f0b
Shanen Cross <shanencross@umail.ucsb.edu>
2013-05-19 23:42:06 -0700

    Added framework for updater for GUI timer class, which has a static method to be called from the main GUI src file

---
+++ src/edu/ucsb/cs56/projects/games/towers_of_hanoi/utility/Updater.java
@@ -0,0 +1,32 @@
+package src.edu.ucsb.cs56.games.towers_of_hanoi.utility;
+
+
+/** Class that main GUI can call to start up timer
+ *  at the beginning of the game
+ *  @author Shanen Cross
+ */
+
+public class Updater {
+
+    // This assumes that we have a Timer with the methods indicated,
+    // but as of now no one has implemented one yet
+    public static void Start {
+
+	Timer a_timer = new Timer();
+	//	
+
+    }
+    
+    // for potential testing
+    public void go() {
+
+    }
+    
+    // for potential testing
+    public static void main(String[] args) {
+	Updater a_updater = new Updater();
+	a_updater.go()
+    }
+
+    
+}
