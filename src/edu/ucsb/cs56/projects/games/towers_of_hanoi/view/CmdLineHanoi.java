94514f710d3e9cb3c5a397d2d348d94c0ec59956
Roeland Singer-Heinze <roeland@umail.ucsb.edu>
2013-05-31 10:47:48 -0700

    Initial commit with nothing implemented yet

---
+++ src/edu/ucsb/cs56/projects/games/towers_of_hanoi/view/CmdLineHanoi.java
@@ -0,0 +1,21 @@
+edu.ucsb.cs56.projects.games.towers_of_hanoi.view;
+
+
+
+public class CmdLineHanoi{
+
+
+
+
+
+    public static void main(String [] args){
+
+	//nothing happens
+
+    }
+
+
+
+
+}
+
