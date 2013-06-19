585ee94b149f00b4c8183fdcdcc41e15206968c6
pconrad <pconrad@cs.ucsb.edu>
2013-06-18 22:32:14 -0700

    remove bad commit

--- src/edu/ucsb/cs56/projects/games/towers_of_hanoi/utility/TimerCallback.java
+++
@@ -1,9 +0,0 @@
-package src.edu.ucsb.cs56.games.towers_of_hanoi.utility;
-
-/** Generic Timer Callback Interface
- *  @author Shanen Cross
- */
-
-public interface TimerCallback {
-    public void callback(Object o);
-}
--- src/edu/ucsb/cs56/projects/games/towers_of_hanoi/utility/TimerPanel.java
+++
@@ -1,8 +0,0 @@
-package edu.ucsb.cs56.projects.games.towers_of_hanoi;
-
-import  javax.swing.*;
-import java.awt.event.*;
-
-public class TimerPanel extends JPanel {
-    
-}
