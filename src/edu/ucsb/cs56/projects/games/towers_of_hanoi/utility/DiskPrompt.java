5b75f929f439627c4df294796a65c1ddf25363dd
Connor Armbrust Mulcahey <connormulcahey@me.com>
2015-02-25 13:33:30 -0800

    cleaned up DiskUitility file, made poorly done placeholder into a label

--- src/edu/ucsb/cs56/projects/games/towers_of_hanoi/utility/DiskPrompt.java
+++ src/edu/ucsb/cs56/projects/games/towers_of_hanoi/utility/DiskPrompt.java
@@ -2,48 +2,55 @@ package edu.ucsb.cs56.projects.games.towers_of_hanoi.utility;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class DiskPrompt extends JFrame{
-
-	private static String diskInput;
-	private static JTextField txt = new JTextField("Number of Disks");
+        
+    private static String diskInput;
+    private static JTextField txt = new JTextField();
+    private static JLabel label = new JLabel("Number of Disks");
+    	
+    public DiskPrompt(){
+	super("How many disks?");
 	
-	public DiskPrompt(){
-		super("How many disks?");
-		
-		JButton doneButton = new JButton("Done");
-		doneButton.addActionListener(new doneButtonListener());
-		this.add(txt,BorderLayout.NORTH);
-		this.add(doneButton,BorderLayout.SOUTH);
-		this.setPreferredSize(new Dimension(300, 100));
-		this.pack();
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setVisible(true);
-	}
+	JButton playButton = new JButton("Play");
+	playButton.addActionListener(new playButtonListener());
+	this.add(label, BorderLayout.NORTH);
+	this.add(txt);
+	//this.add(disksSlider, BorderLayout.NORTH);
+	this.add(playButton,BorderLayout.SOUTH);
+	this.setPreferredSize(new Dimension(300, 100));
+	this.pack();
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	this.setVisible(true);
 	
-	
-	
-	public String getString(){
-		return diskInput;
-	}
-	
-	private class doneButtonListener implements ActionListener{
-
+	// Focus the text field so the user doesn't have to click it
+	EventQueue.invokeLater(new Runnable() {
 		@Override
-		public void actionPerformed(ActionEvent e) {
-            diskInput = txt.getText();
-            System.out.println(diskInput);
-            //txt.setText("");
-           //System.out.println(diskInput);  
-			
+		public void run() {
+		    txt.grabFocus();
+		    txt.requestFocus();
 		}
-		
-	}
+	    });
+    }
+    
+    public String getString(){
+	return diskInput;
+    }
+    
+    private class playButtonListener implements ActionListener{
 	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	    diskInput = txt.getText();
+	    System.out.println(diskInput);			
+	}	
+    }    
 }
