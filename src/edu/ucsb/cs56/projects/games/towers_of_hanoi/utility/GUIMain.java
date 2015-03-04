1c4fe772864d85887ae0d07255036b66ea7ac062
Connor Armbrust Mulcahey <connormulcahey@me.com>
2015-03-04 15:21:00 -0800

    fixed indentation

--- src/edu/ucsb/cs56/projects/games/towers_of_hanoi/utility/GUIMain.java
+++ src/edu/ucsb/cs56/projects/games/towers_of_hanoi/utility/GUIMain.java
@@ -15,33 +15,33 @@ import edu.ucsb.cs56.projects.games.towers_of_hanoi.model.TowersOfHanoiState;
  */
 public class GUIMain {
 	
-public static void main (String [] args){
-				
-				DiskPrompt prompt = new DiskPrompt();//pop-up that asks for the number of disks
-				
-				//String input = prompt.getString();
-				int disks = 0;
-				while(disks < 3){//loop checks the input from prompt every second to check if it is valid (valid is if it is >= 3)
-					try {
-						Thread.sleep(1000);//1 second delay to keep CPU usage down
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					if(prompt.getString()==null)continue;
-					try {
-	            		disks = Integer.parseInt(prompt.getString());
-	            	    } catch (NumberFormatException e) {
-	            		disks = 0;
-	            		continue;
-	            	    }					
-				}
-				prompt.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);//sets close operation so it doesn't kill the program when we close the frame
-				WindowEvent wev = new WindowEvent(prompt, WindowEvent.WINDOW_CLOSING);//creates close event
-                Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(wev);//closes prompt
-				GameGUI gui = new GameGUI();
-				gui.setState(new TowersOfHanoiState(disks));
+    public static void main (String [] args){
+	
+	DiskPrompt prompt = new DiskPrompt();//pop-up that asks for the number of disks
+	
+	//String input = prompt.getString();
+	int disks = 0;
+	while(disks < 3){//loop checks the input from prompt every second to check if it is valid (valid is if it is >= 3)
+	    try {
+		Thread.sleep(1000);//1 second delay to keep CPU usage down
+	    } catch (InterruptedException e) {
+		e.printStackTrace();
 	}
-
+	    if(prompt.getString()==null)
+		continue;
+	    
+	    try {
+		disks = Integer.parseInt(prompt.getString());
+	    } catch (NumberFormatException e) {
+		disks = 0;
+		continue;
+	    }					
+	}
 	
-	
+	prompt.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);//sets close operation so it doesn't kill the program when we close the frame
+	WindowEvent wev = new WindowEvent(prompt, WindowEvent.WINDOW_CLOSING);//creates close event
+	Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(wev);//closes prompt
+	GameGUI gui = new GameGUI();
+	gui.setState(new TowersOfHanoiState(disks));
+    }    
 }
