8fefff69e9a69bfc3f89b4b52ff793daf64bd7aa
Matthew Cheung <matthewcheung@umail.ucsb.edu>
2016-03-10 00:21:49 -0800

    ActionListener for Pause Button implemented, now must fix paused time

--- src/edu/ucsb/cs56/projects/games/towers_of_hanoi/utility/GamePanel.java
+++ src/edu/ucsb/cs56/projects/games/towers_of_hanoi/utility/GamePanel.java
@@ -19,7 +19,7 @@ import edu.ucsb.cs56.projects.games.towers_of_hanoi.model.TowersOfHanoiState.Tow
  */
 
 public class GamePanel extends JPanel {
-    
+
     private final int DISK_HEIGHT;//height of each disk
     private final int DISK_OFFSET;//vertical space between each disk
     private final int TOWER_WIDTH;//Width of the tower
@@ -39,149 +39,182 @@ public class GamePanel extends JPanel {
     private HanoiTimer timer;
     
     public GamePanel() {
-	super();
-	
-	TOWER_WIDTH = 80;
-	DISK_HEIGHT = 10;
-	TOWER_OFFSET = 20;
-	DISK_OFFSET = 20;
-	INITIAL_OFFSET = 50;
-	
-	sideOffset = (this.getWidth()-(3*TOWER_WIDTH) - (2 * TOWER_OFFSET))/2;
-	
+    	super();
+
+    	TOWER_WIDTH = 80;
+    	DISK_HEIGHT = 10;
+    	TOWER_OFFSET = 20;
+    	DISK_OFFSET = 20;
+    	INITIAL_OFFSET = 50;
+
+    	sideOffset = (this.getWidth()-(3*TOWER_WIDTH) - (2 * TOWER_OFFSET))/2;
+
     }
     
     public void paintComponent(Graphics g) {
-	super.paintComponent(g);
-	
-	if (towers == null) {
-	    return;
-	} else {
-	    sideOffset = (this.getWidth()-(3*TOWER_WIDTH) - (2 * TOWER_OFFSET))/2;
-	    
-	    for(int tower = 1; tower <= towers.size(); tower++) {
-		g.setColor(TOWER_COLOR);
-		
+    	super.paintComponent(g);
+
+   		GameGUI.pauseTimer.addMouseListener(new PauseTimerListener());
+
+    	if (towers == null) {
+    		return;
+    	} else {
+    		sideOffset = (this.getWidth()-(3*TOWER_WIDTH) - (2 * TOWER_OFFSET))/2;
+
+    		for(int tower = 1; tower <= towers.size(); tower++) {
+    			g.setColor(TOWER_COLOR);
+
 		// X coordinate of this tower
-		int towerX = sideOffset + (TOWER_WIDTH+TOWER_OFFSET)*(tower-1);
-		g.fillRect(towerX,INITIAL_OFFSET,TOWER_WIDTH,towerHeight);
-		
-		if (tower == 1) {
-		    if (tower1 == null) {
-			this.tower1 = new TowerPanel();
-			tower1.addMouseListener(new TowerPanelListener(1));
-		    }
-		    
-		    tower1.setBounds(towerX,INITIAL_OFFSET,TOWER_WIDTH,towerHeight);
-		    this.tower1.setOpaque(false);
-		    this.add(this.tower1); 
-		} else if (tower == 2) {
-		    if (tower2 == null){
-			this.tower2 = new TowerPanel();
-			tower2.addMouseListener(new TowerPanelListener(2));
-		    } 
-		    
-		    tower2.setBounds(towerX,INITIAL_OFFSET,TOWER_WIDTH,towerHeight);
-		    this.tower2.setOpaque(false);
-		    this.add(this.tower2);
-		} else {
-		    if (tower3 == null) {
-			this.tower3 = new TowerPanel();
-			tower3.addMouseListener(new TowerPanelListener(3));
-		    }
-		    
-		    tower3.setBounds(towerX,INITIAL_OFFSET,TOWER_WIDTH,towerHeight);
-		    this.tower3.setOpaque(false);
-		    this.add(this.tower3);
-		}
-		
-		for (int j=towers.get(tower-1).size();j > 0;j--) {
-		    int offset =  DISK_OFFSET*((towers.get(tower-1).size()-j)-1);
-		    int y = (towerHeight + TOWER_OFFSET) - offset;
-		    
+    			int towerX = sideOffset + (TOWER_WIDTH+TOWER_OFFSET)*(tower-1);
+    			g.fillRect(towerX,INITIAL_OFFSET,TOWER_WIDTH,towerHeight);
+
+    			if (tower == 1) {
+    				if (tower1 == null) {
+    					this.tower1 = new TowerPanel();
+    					tower1.addMouseListener(new TowerPanelListener(1));
+    				}
+
+    				tower1.setBounds(towerX,INITIAL_OFFSET,TOWER_WIDTH,towerHeight);
+    				this.tower1.setOpaque(false);
+    				this.add(this.tower1); 
+    			} else if (tower == 2) {
+    				if (tower2 == null){
+    					this.tower2 = new TowerPanel();
+    					tower2.addMouseListener(new TowerPanelListener(2));
+    				} 
+
+    				tower2.setBounds(towerX,INITIAL_OFFSET,TOWER_WIDTH,towerHeight);
+    				this.tower2.setOpaque(false);
+    				this.add(this.tower2);
+    			} else {
+    				if (tower3 == null) {
+    					this.tower3 = new TowerPanel();
+    					tower3.addMouseListener(new TowerPanelListener(3));
+    				}
+
+    				tower3.setBounds(towerX,INITIAL_OFFSET,TOWER_WIDTH,towerHeight);
+    				this.tower3.setOpaque(false);
+    				this.add(this.tower3);
+    			}
+
+    			for (int j=towers.get(tower-1).size();j > 0;j--) {
+    				int offset =  DISK_OFFSET*((towers.get(tower-1).size()-j)-1);
+    				int y = (towerHeight + TOWER_OFFSET) - offset;
+
 		    //This is: (max disk width)-(ratio of width : # of disks)*(biggest disk value - this disk's value).  
 		    // Makes the bar width scale with # of disks and size of tower
-		    int diskWidth = TOWER_WIDTH - (TOWER_WIDTH/(maxDisk+1))*(maxDisk- towers.get(tower-1).get(j-1)); 
-		    
-		    int x = towerX+((TOWER_WIDTH-diskWidth)/2);
-		    g.setColor(DISK_COLOR);
-		    g.fillRect(x,y,diskWidth,DISK_HEIGHT);
-		}
-	    }
-	}
+    				int diskWidth = TOWER_WIDTH - (TOWER_WIDTH/(maxDisk+1))*(maxDisk- towers.get(tower-1).get(j-1)); 
+
+    				int x = towerX+((TOWER_WIDTH-diskWidth)/2);
+    				g.setColor(DISK_COLOR);
+    				g.fillRect(x,y,diskWidth,DISK_HEIGHT);
+    			}
+    		}
+    	}
     }
     
     public void setState(TowersOfHanoiState s) {
-	
-	state = s;
-	towers = s.getTowers();
-	
+
+    	state = s;
+    	towers = s.getTowers();
+
 	// This assigns maxBar to the # of bars - 1 which is the same as the max # that represents a bar 
 	// (ie: max possible value returned by towers.get(a).get(b) )
-	maxDisk = towers.get(0).size()-1;
-	towerHeight = (maxDisk + 1) * 2 * DISK_HEIGHT;
+    	maxDisk = towers.get(0).size()-1;
+    	towerHeight = (maxDisk + 1) * 2 * DISK_HEIGHT;
     }
     
     public void setTimer(HanoiTimer timer) {
-	this.timer = timer;
+    	this.timer = timer;
     }
     
+    public class PauseTimerListener implements MouseListener {
+    	@Override
+    	public void mouseClicked(MouseEvent e) {
+    		if(GameGUI.pauseTimer.getText().equals(GameGUI.PAUSE_STR_LIT)){
+    			timer.pause();
+    			GameGUI.pauseTimer.setText(GameGUI.RESUME_STR_LIT);
+    			return;
+    		}
+
+    		else if(GameGUI.pauseTimer.getText().equals(GameGUI.RESUME_STR_LIT)){
+    			timer.resume();
+    			GameGUI.pauseTimer.setText(GameGUI.PAUSE_STR_LIT);
+    			return;
+    		}
+    	}
+
+    	@Override
+    	public void mouseReleased(MouseEvent e) {
+    	}
+    	@Override
+    	public void mousePressed(MouseEvent e) {
+    	}
+    	@Override
+    	public void mouseExited(MouseEvent e) {
+    	}
+    	@Override
+    	public void mouseEntered(MouseEvent e) {
+    	}
+    }
+
+
     private class TowerPanelListener implements MouseListener {
-	int selectedTower;
-	
-	
-	public TowerPanelListener(int selectedTower){
-	    this.selectedTower = selectedTower;
-	}
-	
-	@Override
-        public void mouseClicked(MouseEvent e) {
-	    if(GameGUI.pauseTimer.getText().equals(GameGUI.PAUSE_STR_LIT)){
-		timer.pause();
-		GameGUI.pauseTimer.setText(GameGUI.RESUME_STR_LIT);
-	    }
+    	int selectedTower;
 
-	    else if(GameGUI.pauseTimer.getText().equals(GameGUI.RESUME_STR_LIT)){
-		timer.resume();
-                GameGUI.pauseTimer.setText(GameGUI.PAUSE_STR_LIT);
-            }
 
-            if(fromTower == 0) {
-		fromTower = selectedTower;
-		return;
-	    }
-	    
-	    if(toTower == 0){
-		toTower = selectedTower;
+    	public TowerPanelListener(int selectedTower){
+    		this.selectedTower = selectedTower;
+    	}
 
-		try {
+    	@Override
+    	public void mouseClicked(MouseEvent e) {
+    		// if(GameGUI.pauseTimer.getText().equals(GameGUI.PAUSE_STR_LIT)){
+    		// 	timer.pause();
+    		// 	GameGUI.pauseTimer.setText(GameGUI.RESUME_STR_LIT);
+    		// }
+
+    		// else if(GameGUI.pauseTimer.getText().equals(GameGUI.RESUME_STR_LIT)){
+    		// 	timer.resume();
+    		// 	GameGUI.pauseTimer.setText(GameGUI.PAUSE_STR_LIT);
+    		// }
+
+    		if(fromTower == 0) {
+    			fromTower = selectedTower;
+    			return;
+    		}
+
+    		if(toTower == 0){
+    			toTower = selectedTower;
+
+    			try {
 		    // -1 because the game calls the towers by 1-3 while the code calls them 0-2.  This also allows for 0 to represent unassigned for this method
-		    state.makeMove(fromTower-1, toTower-1);
-		    GameGUI.countDisplay.setText(state.getNumOfMoves()+"");
+    				state.makeMove(fromTower-1, toTower-1);
+    				GameGUI.countDisplay.setText(state.getNumOfMoves()+"");
 		    //GameGUI.gamePanel.add(GameGUI.countDisplay, BorderLayout.CENTER);
-		} catch (TowersOfHanoiIllegalMoveException ex) {
-		    JOptionPane.showMessageDialog(null, "This is an illegal move. You may not place a larger disk on top of a smaller disk.\nPlease place a smaller disk on a larger disk or place it on top of an empty tower.", "Illegal Move", JOptionPane.ERROR_MESSAGE);
-		}
-		
-		
-		
-		GamePanel.this.repaint();
-		
+    			} catch (TowersOfHanoiIllegalMoveException ex) {
+    				JOptionPane.showMessageDialog(null, "This is an illegal move. You may not place a larger disk on top of a smaller disk.\nPlease place a smaller disk on a larger disk or place it on top of an empty tower.", "Illegal Move", JOptionPane.ERROR_MESSAGE);
+    			}
+
+
+
+    			GamePanel.this.repaint();
+
 		// Check to see if game solved
-		if (state.solved()) {
-		    state.handleWin();
-		    
-		    if (timer != null) {
-			timer.stop();
-		    }
-		    
-		    Object[] options =  { "Replay", "Quit Game" };
-		    
+    			if (state.solved()) {
+    				state.handleWin();
+
+    				if (timer != null) {
+    					timer.stop();
+    				}
+
+    				Object[] options =  { "Replay", "Quit Game" };
+
 		    int optimalSolution = (int) Math.pow(2,state.numOfDisks) - 1; // 2^n - 1 is the optimal solution for the game
 		    
 		    String winMessage = "Congratulations! You won the game in " + state.getNumOfMoves() + " moves!\n Optimal solution would have been " + optimalSolution + " moves.";
 		    
-			
+
 		    JOptionPane pane = new JOptionPane(winMessage, JOptionPane.PLAIN_MESSAGE, JOptionPane.DEFAULT_OPTION, null, options);
 		    
 		    JDialog dialog = pane.createDialog(GamePanel.this, "Towers of Hanoi");
@@ -190,35 +223,35 @@ public class GamePanel extends JPanel {
 		    Object selectedValue = pane.getValue();
 		    
 		    if (selectedValue.equals("Replay")) {
-			System.out.println("Selected Replay");
-			GUIMain.startGame();
+		    	System.out.println("Selected Replay");
+		    	GUIMain.startGame();
 		    } else {
-			System.out.println("Selected Quit");
-			System.exit(0);
+		    	System.out.println("Selected Quit");
+		    	System.exit(0);
 		    }
 		}
 		
 		toTower = 0;  fromTower = 0;
 		
 		return;
-	    }
-	    
-	    toTower = 0;  fromTower = 0;
-        }
-	
-        @Override
-        public void mouseReleased(MouseEvent e) {
-        }
-        @Override
-        public void mousePressed(MouseEvent e) {
-        }
-        @Override
-        public void mouseExited(MouseEvent e) {
-        }
-        @Override
-        public void mouseEntered(MouseEvent e) {
-        }
-	
-    }
+	}
+
+	toTower = 0;  fromTower = 0;
+}
+
+@Override
+public void mouseReleased(MouseEvent e) {
+}
+@Override
+public void mousePressed(MouseEvent e) {
+}
+@Override
+public void mouseExited(MouseEvent e) {
+}
+@Override
+public void mouseEntered(MouseEvent e) {
+}
+
+}
 }
 
--- src/edu/ucsb/cs56/projects/games/towers_of_hanoi/utility/HanoiTimer.java
+++ src/edu/ucsb/cs56/projects/games/towers_of_hanoi/utility/HanoiTimer.java
@@ -13,88 +13,91 @@ import javax.swing.JLabel;
  */
 public class HanoiTimer {
     private GregorianCalendar gc = new GregorianCalendar();
-    private GregorianCalendar pt = new GregorianCalendar();
     private long startTime = 0;
     private long pauseTime = 0;
     private JLabel timeLabel = null;
     private boolean stopped = true;
     private boolean paused = false;
+    private boolean started = false;
     /**
      * Consructs a new HanoiTimer and starts the timer running.
      * @param label The label that will receive the formatted elapsed time.
      */
     public HanoiTimer(JLabel label) {
-	timeLabel=label;
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-		
-		@Override
-		public void run() {
-		    updateTimer();
-		}
-	    }, 0, 1000);
-        start();
+     timeLabel = label;
+     Timer timer = new Timer();
+     timer.schedule(new TimerTask() {
+
+      @Override
+      public void run() {
+          updateTimer();
+      }
+    }, 0, 1000);
+     start();
     }
-    
+
     public void updateTimer() {
-	if(timeLabel != null) {
-	    this.SetTimeElapsedText();
-	}
+       if(timeLabel != null) {
+           this.SetTimeElapsedText();
+       }
     }
-    
+
     /**
      * Stops the timer then restarts it.
      */
     public void restart() { 
-	this.stop();
-	this.start();
-    }
-    
+       this.stop();
+       this.start();
+   }
+
     /**
      * If stopped, the timer will restart. Otherwise, do nothing
      */
     public void start() {
-	if(!stopped)return;
-	startTime = System.currentTimeMillis();
-	stopped = false;
-	paused = false;
-    }
-    
+       if(!stopped)return;
+       startTime = System.currentTimeMillis();
+       stopped = false;
+       paused = false;
+       started = true;
+   }
+
     /**
      * Stops processing of timer events
      */
     public void stop() {
-	stopped = true;	
+       stopped = true;	
     }
 
     public void pause() {
-	paused = true;
+        pauseTime = System.currentTimeMillis() - startTime;
+        paused = true;
+        started = false;
     }
     public void resume() {
-	pauseTime = pt.get(GregorianCalendar.SECOND) + 60*pt.get(GregorianCalendar.MINUTE) - startTime;
-	stopped = false;
-	paused = false;
+        stopped = false;
+        paused = false;
     }
-    
+
     /**
      * Sets the label text of the JLabel to the elapsed time in proper format
      */
     public void SetTimeElapsedText() {
-	if(stopped || timeLabel == null)
-	    return;
-	else if(paused == true){
-	    pt.setTimeInMillis(System.currentTimeMillis());	
-	    //pauseTime = System.currentTimeMillis() - startTime;
-	}
-	
-	//	if(stopped || timeLabel == null)
-	//	    return;
-	
-	else{
-	    gc.setTimeInMillis(System.currentTimeMillis() - startTime - pauseTime);
-	    String time = String.format("%02d", gc.get(GregorianCalendar.MINUTE)) 
-		+ ":" + String.format("%02d", gc.get(GregorianCalendar.SECOND));
-	    timeLabel.setText(time);
-	}   
+        if(stopped == true || timeLabel == null)
+            return;
+        else if(paused == false && stopped == false && started == true){
+            gc.setTimeInMillis(System.currentTimeMillis() - startTime);
+        }
+        else if(paused == true && started == false){
+            gc.setTimeInMillis(pauseTime);
+        }
+        else{
+             gc.setTimeInMillis(System.currentTimeMillis() - pauseTime - startTime);
+        }
+
+        String time = String.format("%02d", gc.get(GregorianCalendar.MINUTE)) 
+        + ":" + String.format("%02d", gc.get(GregorianCalendar.SECOND));
+        timeLabel.setText(time);
     }
 }
+
+
