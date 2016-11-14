6ce69337b05ca4221fe1efb05aa24ae3d1a07a9c
Zizhuo Lin <zizhuo_lin@umail.ucsb.edu>
2016-11-14 14:28:27 -0800

    update #48

---
+++ src/edu/ucsb/cs56/projects/games/towers_of_hanoi/utility/GamesettingFrame.java
@@ -0,0 +1,51 @@
+package edu.ucsb.cs56.projects.games.towers_of_hanoi.utility;
+import java.awt.Toolkit;
+import java.awt.event.WindowEvent;
+import java.io.*;
+import javax.swing.*;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.*;
+
+public class GamesettingFrame{
+    GameSetting gamesetting;
+    public void setSetting(GameSetting s)
+    {this.gamesetting = s;}
+    public void go(){
+	JFrame settingFrame = new JFrame("Setting");
+		    settingFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		    JButton instruction = new JButton("Instruction");
+		    JLabel OnOrOff = new JLabel();
+		    if (gamesetting.getInstruction())
+			OnOrOff.setText("On");
+		    else
+			OnOrOff.setText("Off");
+		    instruction.addActionListener(new ActionListener(){
+			    public void actionPerformed(ActionEvent e)
+			    {if (OnOrOff.getText() == "On")
+				    {OnOrOff.setText("Off");
+					gamesetting.setInstruction(false);}
+				else
+				    {OnOrOff.setText("On");
+					gamesetting.setInstruction(true);}
+			    }
+			});
+		    JButton Save = new JButton("Save");
+		    Save.addActionListener(new ActionListener(){
+			    public void actionPerformed(ActionEvent e)
+			    {try{
+				FileOutputStream fos = new FileOutputStream("GameSetting.ser");
+				ObjectOutputStream os = new ObjectOutputStream(fos);
+				os.writeObject(gamesetting);
+				os.close();}
+				catch(IOException ex){ex.printStackTrace();}}
+			});
+		    settingFrame.setLayout(new GridLayout(2,2,20,10));
+		    settingFrame.add(instruction);
+		    settingFrame.add(OnOrOff);
+		    settingFrame.add(Save);
+		    settingFrame.setSize(400,100);
+		    settingFrame.setLocationRelativeTo(null);
+		    settingFrame.setVisible(true);}}
